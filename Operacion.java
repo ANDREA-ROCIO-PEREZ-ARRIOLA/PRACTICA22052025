25480f6b7a69826007abde6f14e88aa697d31522
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-22 08:46:30 -0600

    avance en la clase de operacion

---
+++ Operacion.java
@@ -0,0 +1,21 @@
+public class Operacion {
+
+public int Sumar(int a, int b) {
+if(a<0 || b<0){
+    System.out.println("ERROR: No se puede sumar numeros negativos");
+    return -1;
+}
+    return a + b;
+}
+
+public double Sumar(double a, double b) {
+    return a + b;
+}
+
+public int Sumar(int a, int b, int c) {
+    return a + b + c;
+}
+
+
+
+}
